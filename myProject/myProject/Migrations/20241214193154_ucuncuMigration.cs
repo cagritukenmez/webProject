b55e4fd09f2e2c813e232e0825fd0ddded9fd8ab
cagritukenmez <117526414+cagritukenmez@users.noreply.github.com>
2024-12-14 22:40:24 +0300

    migration dosyası güncelleme
    Yeni migration oluşturulup veritabanı güncellendi. Eklenen sınıf: kullanıcı sınıfı.

---
+++ myProject/myProject/Migrations/20241214193154_ucuncuMigration.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace myProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class ucuncuMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Kullanıcı",
+                columns: table => new
+                {
+                    kullaniciID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    kullaniciAdi = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: false),
+                    rol = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Kullanıcı", x => x.kullaniciID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Kullanıcı");
+        }
+    }
+}
