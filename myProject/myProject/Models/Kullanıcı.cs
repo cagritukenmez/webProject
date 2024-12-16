5f2694495a1e05bab7d129186eb002547b18519c
cagritukenmez <117526414+cagritukenmez@users.noreply.github.com>
2024-12-16 21:52:53 +0300

    Kullanıcı güncelleme
    Kullanıcı modeline eposta özelliği eklenerek güncellenmiştir.

--- myProject/myProject/Models/Kullanıcı.cs
+++ myProject/myProject/Models/Kullanıcı.cs
@@ -7,12 +7,16 @@ namespace myProject.Models
         public int kullaniciID { get; set; }
         [Required]
         [StringLength(15,MinimumLength =3,ErrorMessage ="Şifre en az 3 karakter uzunluğunda olmalıdır.")]
-        [Display(Name ="Kullanıcı Adı ")]
+        [Display(Name ="Kullanıcı Adı :")]
         public string kullaniciAdi { get; set; }
         [Required]
         [StringLength(15,MinimumLength =8,ErrorMessage = "Şifre en az 8 karakter uzunluğunda olmalıdır.")]
-        [Display(Name ="Şifre")]
-        private string kullaniciSifre { get; set; }
+        [Display(Name ="Şifre :")]
+        public string kullaniciSifre { get; set; }
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Eposta :")]
+        public string eposta { get; set; }
         public string rol { get; set; }
     }
 }
