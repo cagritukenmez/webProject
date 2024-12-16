487797bb128e8f8a3cf93954e54094d86dce9a7b
cagritukenmez <117526414+cagritukenmez@users.noreply.github.com>
2024-12-16 21:51:48 +0300

    Migration güncelleme
    Kullanıcı modeline eposta eklendi bununla birlikte yeni bir migration oluşturuldu ve veritabanı güncellendi.

--- myProject/myProject/Migrations/20241216180750_dorduncuMigration.Designer.cs
+++ myProject/myProject/Migrations/20241216180750_dorduncuMigration.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using myProject.Models;
 
@@ -11,9 +12,11 @@ using myProject.Models;
 namespace myProject.Migrations
 {
     [DbContext(typeof(myyDbContext))]
-    partial class myyDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241216180750_dorduncuMigration")]
+    partial class dorduncuMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ myProject/myProject/Migrations/20241216180750_dorduncuMigration.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace myProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class dorduncuMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "eposta",
+                table: "Kullanıcı",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "kullaniciSifre",
+                table: "Kullanıcı",
+                type: "nvarchar(15)",
+                maxLength: 15,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "eposta",
+                table: "Kullanıcı");
+
+            migrationBuilder.DropColumn(
+                name: "kullaniciSifre",
+                table: "Kullanıcı");
+        }
+    }
+}
