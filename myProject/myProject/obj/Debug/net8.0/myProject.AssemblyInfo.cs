b3c02c1b6307182b55b59fbcecc6a47a20b0fca7
Çağrı Tükenmez <grfspear@gmail.com>
2024-12-27 01:32:07 +0300

    güncelleme
    genel güncelleme

--- myProject/myProject/obj/Debug/net8.0/myProject.AssemblyInfo.cs
+++ myProject/myProject/obj/Debug/net8.0/myProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("myProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3cff93d067c38602c65ea141210e609bdddb8e94")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5df35d2358b6a6c24d9edb07c4e800684efb92ef")]
 [assembly: System.Reflection.AssemblyProductAttribute("myProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("myProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
